211210d28eb554443b83ae4d7ee5a80407661aef
Eder Ávila Prado <prado.eder@gmail.com>
2018-04-05 14:59:39 -0300

    read and write data

---
+++ index.js
@@ -0,0 +1,29 @@
+// load fake virtual env
+require("dotenv").config();
+
+const fs = require('fs');
+
+console.log('-----------');
+console.log("RAPPI_INPUT_METADATA_PATH:", process.env.RAPPI_INPUT_METADATA_PATH);
+console.log("RAPPI_INPUT_DATA_PATH:", process.env.RAPPI_INPUT_DATA_PATH);
+console.log("RAPPI_OUTPUT_METADATA_PATH:", process.env.RAPPI_OUTPUT_METADATA_PATH);
+console.log("RAPPI_OUTPUT_DATA_PATH:", process.env.RAPPI_OUTPUT_DATA_PATH);
+console.log('-----------');
+
+const inpMetadata = require(process.env.RAPPI_INPUT_METADATA_PATH),
+      inpData = require(process.env.RAPPI_INPUT_DATA_PATH);
+  
+console.log('input metadata:', inpMetadata);
+console.log("input data:", inpData);
+
+// Changing metadata and data...
+const outMetadata = Object.assign(inpMetadata, { ts: Date.now() }),
+      outData = [{ ts: Date.now() }].concat(inpData, inpData, inpData);
+
+console.log("output metadata:", outMetadata);
+console.log("output data:", outData);
+
+fs.writeFileSync(process.env.RAPPI_OUTPUT_METADATA_PATH, JSON.stringify(outMetadata, null, 2));
+fs.writeFileSync(process.env.RAPPI_OUTPUT_DATA_PATH, JSON.stringify(outData, null, 2));
+
+console.log('->', 'end...');
